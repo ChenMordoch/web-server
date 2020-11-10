624400929f301dbec6e00ae47b74370161c1550e
Hen Mordoch <hen.mord@gmail.com>
2020-11-11 01:36:59 +0200

    refactoring

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 const PORT = 3000;
 
 var app = express();
-
-var middleware = {
-  requireAuthentication: function(req, res, next) {
-    console.log('private route hit!');
-    next();
-  },
-  logger: function(req, res, next) {
-      console.log('request' + new Date.toString() + ' ' + req.method + ' ' + req.originalUrl);
-      next();
-  }
-};
+var middleware = require('./middleware');
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
